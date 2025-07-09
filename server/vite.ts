bd33d35d23f49d2ca18734738bf5af4e0cd892b3
Geektrepreneur <rfavors@users.noreply.github.com>
2025-07-09 16:46:32 -0500

    Fixed path.resolve

--- server/vite.ts
+++ server/vite.ts
@@ -46,11 +46,12 @@ export async function setupVite(app: Express, server: Server) {
 
     try {
       const clientTemplate = path.resolve(
-        import.meta.dirname,
-        "..",
-        "client",
-        "index.html",
-      );
+  __dirname,
+  "..",
+  "client",
+  "index.html"
+);
+
 
       // always reload the index.html file from disk incase it changes
       let template = await fs.promises.readFile(clientTemplate, "utf-8");
